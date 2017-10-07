7871de7362f500b90af4f63de90ae709e403c6e9
John Soward <soward@soward.net>
2017-10-06 22:56:33 -0400

    Initial Commit

---
+++ sxmInfo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sxmInfo
+//
+//  Created by John Soward on 10/6/17.
+//  Copyright © 2017 soward.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
